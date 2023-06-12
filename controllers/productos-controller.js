16873c4ce58aacbf0bff29571b1d0f324583341e
RoDFS-GH <rosana.19abril@gmail.com>
2023-06-12 13:56:55 -0300

    actualizar producto-servicios

--- controllers/productos-controller.js
+++ controllers/productos-controller.js
@@ -1,4 +1,4 @@
-import { productoServices } from "../servicios/producto-servicios.js";
+import { productoServices } from "./servicios/producto-servicios.js";
 import { formatPrice } from "../formatterPrices.js";
 
 const nuevoProducto = (name, price, imageUrl, id) => {
